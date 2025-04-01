d8e5cdd9bfb8b39281f82bf4744177259c5f05be
akshatale <akshat.cd22@bmsce.ac.in>
2025-04-01 10:21:22 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
-print("hello akshat ale")
-print("this is a change")
-print("this is the local change")
+print("hello akshat ale");
+print("this is a change");
+print("this is the local change");
+print("testing new branch");
+print("jenkins test job");
