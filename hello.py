f8982968f359475d1b04114f17f6aad9693110f5
akshatale <akshat.cd22@bmsce.ac.in>
2025-04-08 10:25:07 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,3 +4,4 @@ print("this is the local change");
 print("testing new branch");
 print("jenkins test job");
 print("this is another change for lab exam");
+print("this is another change for lab exam");
